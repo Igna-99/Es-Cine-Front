8cec92aaf7d54d197151974c54317f207775094f
Camila Jaurena <jaurenacamila@gmail.com>
2023-06-27 11:59:38 -0300

    estilos, logo y navbar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <script setup>
-import Navbar from './components/Navbar.vue'
-import { usrStore } from './components/store/usrStore'
-import { onMounted } from 'vue'
 
-onMounted(() => {
-  let store = usrStore()
-  store.cargarDataStorage()
-  
-})
+    import Navbar from './components/Navbar.vue'
+    import { usrStore } from './components/store/usrStore'
+    import { onMounted } from 'vue'
+
+    onMounted(() => {
+        let store = usrStore()
+        store.cargarDataStorage()
+    })
 
 </script>
 
 <template>
+
   <Navbar />
 
   <div class="componentes">
     <RouterView></RouterView>
   </div>
-</template>
-<style>
-@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap");
 
-.componentes {
-  margin-top: 70px;
-}
+</template>
+
+<style>
+
+    @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap");
+
+    .componentes {
+        margin-top: 110px;
+    }
+
 </style>
 
